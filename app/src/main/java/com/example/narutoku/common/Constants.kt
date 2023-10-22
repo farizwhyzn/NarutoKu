09dcbaf088c71dca0e3617905031c4a7f173f134
fariz <farizwhyzn@gmail.com>
2023-10-22 09:02:27 +0700

    Add room db name

--- app/src/main/java/com/example/narutoku/common/Constants.kt
+++ app/src/main/java/com/example/narutoku/common/Constants.kt
@@ -3,4 +3,5 @@ package com.example.narutoku.common
 object Constants {
     const val BASE_URL = "https://narutodb.xyz/api/"
     const val PARAM_CHARACTER_ID = "characterId"
+    const val CHARACTER_DATABASE_NAME = "Character.db"
 }
